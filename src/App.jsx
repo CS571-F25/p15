48f0dffa5f60527bc23d077ce218dac3a489575f
Ryan Pratt <rmpratt@wisc.edu>
2025-11-02 14:25:58 -0600

    NOW Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router'
+import Homepage from './components/Homepage.jsx'
 
 function App() {
   return (
