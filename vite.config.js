b712d96da3f672cd229b6ccf1bb49532b69cd859
Ryan Pratt <rmpratt@wisc.edu>
2025-12-10 19:38:18 -0600

    more config changes, new workflow for github pages and netlify split

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const base = process.env.VITE_BASE ?? '/p15/'
+
 export default defineConfig({
+  base,
   plugins: [react()],
   build: {
     outDir: 'docs'
